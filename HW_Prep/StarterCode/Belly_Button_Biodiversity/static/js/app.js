7a57fa948e9ea1f6247a0e8ee830b3d480fa0f16
ShanaBrand <shanakay.brandford@gmail.com>
2018-09-11 07:02:31 -0700

    Interactive Visualization

--- HW_Prep/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ HW_Prep/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,41 +1,87 @@
+var selection = d3.select("select")
+      		.on("change", function(){
+            console.log("You selected someting",d3.select(this));
+            var sample_sel = d3.select("onchange");
+            buildMetadata(sample_sel);
+
+          });
+          
+
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-  //var metaUrl = "/metadata/<sample>";
-  //console.log(sample.value);
-
+  url = '/metadata/<samples>';
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  d3.json(url).then(function(response){
+    console.log(response)
+  
+    // @TODO: Complete the following function that builds the metadata panel
 
+   // Use d3 to select the panel with id of `#sample-metadata`
+  d3.select("sample-metadata")
+  .html(" ")
     // Use `.html("") to clear any existing metadata
+    
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
+
+
+  
+
+  
+    
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+
+  })
+
+  
+
 
 function buildCharts(sample) {
-  //url = '/sample/<sample>';
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  //d3.json(url).then(function(response){
+  url = "samples/<samples>"
 
-    //console.log(response);
-    //sample = [response];
+  d3.json(url).then(function(response){
+
+    console.log(response);
+
+    var sample_data = [response];
+
 // @TODO: Build a Bubble Chart using the sample data
-   //trace_bub
+    bub_trace = 
 
+    pie_trace = 
+
+
+
+    var layout = {
+      title: "Pet Pals",
+      xaxis: {
+        title: "Pet Type"
+      },
+      yaxis: {
+        title: "Number of Pals"
+      }
+    };
+
+    Plotly.newPlot("plot", data, layout);
+  });
+}
+
+
+
+
+
+
+  })
+    
     // @TODO: Build a Pie Chart
-    //trace_pie
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-
-
-
-    
 }
 
 function init() {
