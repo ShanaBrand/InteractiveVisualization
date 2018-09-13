6205a8a8c13dd42019b2c7bfcbd96d5f06c178b4
ShanaBrand <shanakay.brandford@gmail.com>
2018-09-12 19:59:14 -0700

    WorkingDashboard

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -1,78 +1,95 @@
 function buildMetadata(sample) {
-
   // @TODO: Complete the following function that builds the metadata panel
-  var url = `metadata${sample}`;
+  var url = `/metadata/${sample}`;
+  console.log(url)
   
-
   // Use `d3.json` to fetch the metadata for a sample
- d3.json(url).then(function(data){
-   var data = [data];
-   console.log(data)
-  })
+  d3.json(url).then((data)=>{
+  // var data = [data];
+  // console.log(data)
+  //})
   // Use d3 to select the panel with id of `#sample-metadata`
-  d3.select("#sample-metadata")
-  .html("");
+  metadataPanel =d3.select("#sample-metadata")
   
   // Use `.html("") to clear any existing metadata
+  metadataPanel.html("");
   
-// Use `Object.entries` to add each key and value pair to the panel
-// Hint: Inside the loop, you will need to use d3 to append new
-// tags for each key-value in the metadata.
-Object.entries(data).forEach(([key,value])=>{
-  var selected = Object.entries.append("p");
-  selected.text(`${key}:${value}`);
+  
+  // Use `Object.entries` to add each key and value pair to the panel
+  // Hint: Inside the loop, you will need to use d3 to append new
+  // tags for each key-value in the metadata.
+  Object.entries(data).forEach(([key,value])=>{
+      metadataPanel.append("h6").text(`${key}:${value}`);
     });
-
+  
+    // BONUS: Build the Gauge Chart
+  });
+  }
+  
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-
-}
-
-function buildCharts(sample) {
-
-  url = `samples${sample}`;
-
+  
+  function buildCharts(sample) {
+  urlS = `/samples/${sample}`;
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  d3.json(url).then(function(response){
-    console.log(response);
-    extract = [response];
-    topTen = extract.slice(0,10)
+  d3.json(urlS).then((data) =>{
+  /*   extract = [response];
+    console.log(extract)
+    topTen = extract.slice(0,10) */
+    // Define values
+    const otu_ids = data.otu_ids;
+    const sample_values = data.sample_values;
+    const otu_labels = data.otu_labels;
+    console.log(sample_values);
+
+    const topSamp = sample_values.slice(0,10);
+    const top_id = otu_ids.slice(0,10);
+    const top_labels = otu_labels.slice(0,10);
 
   
   var pie_data = [{
-      values: topTen.sample_values,
-      labels: topTen.otu_ids,
-      hoverinfo: topTen.otu_labels,
+      values: topSamp,
+      labels: top_id,
+      hoverinfo: top_labels,
       type: 'pie',
     }];
-    
+  
     var layout = {
       height: 400,
       width: 500
     };
-    d3.select('#pie')
-    Plotly.newPlot('pie', pie_data, layout);
-    
-
+    pieDiv = d3.select("#pie")
+    //pieDiv.html("");
+    //Plotly.plot('pie', pie_data),layout);
+    Plotly.plot("pie", pie_data, layout);
+  
     var bub_data = [{
-      'x': extract.otu_ids,
-      'y': extract.sample_values,
-      'mode':'markers',
-      'marker':{
-        'size': extract.sample_values,
-        'color': otu_ids,
-        'text':extract.otu_labels
+      x: otu_ids,
+      y: sample_values,
+      mode:'markers',
+      marker:{
+        size: sample_values,
+        color: otu_ids,
+        text: otu_labels
       }
     }];
-    Plotly.newPlot('bubbly', bub_data);
-    // HINT: You will need to use slice() to grab the top 10 sample_values,
-    // otu_ids, and labels (10 each).  
-});
 
-function init() {
+    var bub_layout = {
+      title: 'Belly Button BioDiversity',
+      showlegend: true,
+      height: 600,
+      width: 900
+    }
+
+    bubDiv = d3.select("#bubble")
+    Plotly.plot("bubble", bub_data,bub_layout);
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+  });
+  }
+  function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
-
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
     sampleNames.forEach((sample) => {
@@ -81,21 +98,19 @@ function init() {
         .text(sample)
         .property("value", sample);
     });
-
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
   });
-}
-
-function optionChanged(newSample) {
+  }
+  function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
-}
-
-// Initialize the dashboard
-init();
-
-}
+  }
+  // Initialize the dashboard
+  init();
+  
+  
+  
